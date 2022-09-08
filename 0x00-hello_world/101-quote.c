129337dd6a5e1e6c1ce311c275e64cc6ebc6f9ee
Nginika <nginikaugochukwu@gmail.com>
2022-09-08 15:27:32 -0700

    print to std err

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - print to standard error
+* Description: print texts to standard error instead of stdout
+* Return: 0 after running
+*/
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" -
+	 Dora Korpar, 2015-10-19\n");
+
+	return (0);
+}
