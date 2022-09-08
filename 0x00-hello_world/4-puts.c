5a5137bfcd18adedb3f01de68189392aef2c9244
Nginika <nginikaugochukwu@gmail.com>
2022-09-08 10:48:58 -0700

    fix betty for task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
-int main(void)/*print out text*/
+/**
+* main - this prints out text
+* function - main
+*Description: this just prints a line of text i put there
+* Return: the return value should be zero
+*/
+int main(void) /* return 0 */
 {
 	char Mytext[] = "\"Programming is like building a multilingual puzzle";
 
