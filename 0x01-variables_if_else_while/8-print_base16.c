9966cedd24a82d5c496a5f7b9021d9bdc2654c02
Nginika <nginikaugochukwu@gmail.com>
2022-09-09 14:25:41 -0700

    print base 16 and fix 4 and 6

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
-* main - print single digit number
-* Description: print 0 to 9
+* main - print all hexadecimal numbers ad letters
+* Description: print hexadecimal letters and numbers
 * Return: 0 if executed
 */
 
@@ -10,9 +10,13 @@
 int main(void)
 {
 	int digit;
+	char letter;
 
 	for (digit = 0; digit < 10; ++digit)
 		putchar((digit % 10) + '0');
+
+	for (letter = 'a'; letter <= 'f'; ++letter)
+		putchar(letter);
 
 	putchar('\n');
 
