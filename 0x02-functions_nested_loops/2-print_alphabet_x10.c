1f68a4378a6722c7112bc8f2750e74a40caba6f8
Nginika <nginikaugochukwu@gmail.com>
2022-09-13 15:18:22 +0000

    print alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* print_alphabet - print alphabet using _putchar
+* print_alphabet_x10 - print alphabet using _putchar
 * Description: prints lowercase letters a-z
 */
 
@@ -14,8 +14,9 @@ void print_alphabet_x10(void)
 	{
 		for (letter = 'a'; letter <= 'z'; ++letter)
 			_putchar(letter);
-	}
 
-	_putchar('\n');
+		_putchar('\n');
+
+	}
 
 }
