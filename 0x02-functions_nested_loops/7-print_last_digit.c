8a7cc51bce22a85512e45e8048563506c34ffdc2
Nginika <nginikaugochukwu@gmail.com>
2022-09-13 20:55:02 +0000

    print last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,10 +9,20 @@
 int print_last_digit(int n)
 {
 
-	n = n % 10;
+	if (n >= 0)
+	{
+		n = n % 10;
 
-	_putchar(n + '0');
+		_putchar(n + '0');
 
-	return (n);
+		return (n);
+	}
+	else
+	{
+		n = -1 * (n % 10);
 
+		_putchar(n + '0');
+
+		return (n);
+	}
 }
