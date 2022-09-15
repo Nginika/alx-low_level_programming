387bfd5a91b3da93aa1e5919810d5981f5808bf6
Nginika <nginikaugochukwu@gmail.com>
2022-09-15 20:19:03 +0000

    the fizzbuzz challenge

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,18 +8,20 @@ int main(void)
 {
 	int i = 1;
 
-	while (i < 100)
+	while (i <= 100)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
 		else if	(i % 3 == 0)
 			printf("Fizz ");
 		else if (i % 5 == 0)
-			printf("Buzz ");
+			if (i == 100)
+				printf("Buzz");
+			else
+				printf("Buzz ");
 		else
 			printf("%d ", i);
 		i++;
 	}
-	printf("Buzz");
 	return (0);
 }
