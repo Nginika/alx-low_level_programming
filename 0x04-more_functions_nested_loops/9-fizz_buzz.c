5e4fb4e4f271a0a8f45720dd72a021f5fc4b2ff7
Nginika <nginikaugochukwu@gmail.com>
2022-09-15 19:57:20 +0000

    the fizzbuzz challenge

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print the fizz buzz challenge
+ * Return: 0 always
+*/
+int main(void)
+{
+	int i = 1;
+
+	while (i < 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if	(i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+		i++;
+	}
+	return (0);
+}
