dee2b516aa41333e4c33ce4385203ef750979f08
Nginika <nginikaugochukwu@gmail.com>
2022-09-15 13:38:34 +0000

    print 1 to 14 ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,7 +14,13 @@ void more_numbers(void)
 	{
 		while (i < 15)
 		{
-			_putchar(i + '0');
+			if (i < 10)
+				_putchar(i + '0');
+			else
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
 			i++;
 		}
 		j++;
