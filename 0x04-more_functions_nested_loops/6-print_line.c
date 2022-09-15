648314ac775874cf7fcff497f922bfaf916ecbe6
Nginika <nginikaugochukwu@gmail.com>
2022-09-15 14:28:55 +0000

    print lines and a new line after

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_line - print a line with n underscores
+* @n: number of underscores to print
+* Return: void
+*/
+
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		while (i < n)
+		{
+			_putchar('_');
+			i++;
+		}
+		_putchar('\n');
+	}
+}
