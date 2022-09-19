5a6a064584641c37bcdc0087d5c9f8fa1ddaeb61
Nginika <nginikaugochukwu@gmail.com>
2022-09-19 15:40:04 +0000

    print strings like puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -11,8 +11,10 @@ void _puts(char *str)
 	int len = 0;
 
 	while (str[len] != '\0')
+	{
 		_putchar(str[len]);
 		len++;
+	}
 
 	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 int _putchar(char c);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void print_rev(char *s);
 void _puts(char *str);
 void reset_to_98(int *n);
