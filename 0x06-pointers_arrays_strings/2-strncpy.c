05bc8d65024d3a4a49121981165da52c52101730
Nginika <nginikaugochukwu@gmail.com>
2022-09-22 08:57:42 +0000

    tasks 1 and 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _strncpy - copies string to a number of byte
+* @dest: destination to copy to
+* @src: string to copy
+* @n: number of bytes
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while 
+}
