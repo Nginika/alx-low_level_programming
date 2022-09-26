ba6de75b040eb99c4a1b8df4ee278619699c21c2
Nginika <nginikaugochukwu@gmail.com>
2022-09-26 16:04:43 +0000

    search for that character

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
 * _strchr - search for a character
@@ -21,7 +20,7 @@ char *_strchr(char *s, char c)
 			break;
 		}
 		else
-			n = NULL;
+			n = '\0';
 	}
 	return (n);
 }
