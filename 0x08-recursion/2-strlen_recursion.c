e2c9746a5fd999662e680b1784db33f7fc90dea8
Nginika <nginikaugochukwu@gmail.com>
2022-09-28 13:23:48 +0000

    count the length of a string recursively

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* _strlen_recursion(char *s);
+* _strlen_recursion - counts length of string recursively
 * @s: string to count
 * Return: length of string
 */
@@ -10,7 +10,7 @@ int _strlen_recursion(char *s)
 {
 	if (*s)
 	{
-		return(1 + _strlen_recursion(s + 1));
+		return (1 + _strlen_recursion(s + 1));
 	}
 	return (0);
 }
