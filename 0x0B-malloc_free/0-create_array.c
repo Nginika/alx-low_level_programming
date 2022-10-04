867d8950a5ee76ad8ad4a9ca6058712de6b74ef1
Nginika <nginikaugochukwu@gmail.com>
2022-10-04 12:28:42 +0000

    create an array of characters

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,12 +17,12 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 	{
-		return ('\0');
+		return (NULL);
 	}
 	
-	if (A == '\0')
+	if (A == NULL)
 	{
-		return ('\0')
+		return (NULL);
 	}
 
 	for (i = 0; i < size; i++)
