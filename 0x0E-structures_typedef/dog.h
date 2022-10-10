fe732de9a6aa671e6e2275eb588b081b2272e223
Nginika <nginikaugochukwu@gmail.com>
2022-10-10 17:01:42 +0000

    struct for dog with three variables

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_NAME
+#define DOG_NAME
+
+/**
+* struct dog - characteristics of a dog
+* @name: dog's name
+* @age: dog's age
+* @owner: owner of dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
