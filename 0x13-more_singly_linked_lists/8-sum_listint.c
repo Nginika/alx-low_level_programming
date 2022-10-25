59935277bbfb7d2e1448a663bd8f3b62cab1a5fc
Nginika <nginikaugochukwu@gmail.com>
2022-10-25 15:40:55 +0000

    add all n values in linked list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * sum_listint - adds all n values in the listint_t list
+ * @head: the pointer in the head node
+ * Return: sum of all n values, 0 for empty list
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	
+	while (head)
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
+	return (sum);
+}
