232059327b69a8cc5ea5653535221b1ef199c069
Nginika <nginikaugochukwu@gmail.com>
2022-10-31 19:16:59 +0000

    create a file using the open system call and write in it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,16 +15,19 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 00600);
+	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (file == -1)
 		return (-1);
 
-	for (len = 0; text_content[len]; len++)
+	if (text content != NULL)
 	{
-		fwrite = write(file, text_content, len);
+		for (len = 0; text_content[len]; len++)
+		{
+			fwrite = write(file, text_content, len);
 
-		if (fwrite == -1)
-			return (-1);
+			if (fwrite == -1)
+				return (-1);
+		}
 	}
 	close(file);
 	return (1);
