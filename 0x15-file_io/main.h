fd590f79fdd936b52b10c48d0b0f8b42499dcd82
Nginika <nginikaugochukwu@gmail.com>
2022-10-31 19:11:35 +0000

    create a file using the open system call and write in it

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif /* FILE_IO */
